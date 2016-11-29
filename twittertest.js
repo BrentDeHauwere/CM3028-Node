73cf44bce3c33188c77d7fcc2d71b589ca714d13
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-11-29 11:51:07 +0000

    Twitter

---
+++ twittertest.js
@@ -0,0 +1,20 @@
+// The first thing we need to do is let node.js know we require the Twitter library
+var Twitter = require('twitter');
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+var client = new Twitter({
+	consumer_key: 'LULonHhwbfxqolOc3XcB1EMV7',
+	consumer_secret: 'ESaYdaqdNklLGLn1VfRlrOKek86crvTca1ydewNqYb2R8oykdM',
+	access_token_key: '464137167-tF6rYBOrEgfuZ1IVSR0z1r3MESUNgLJx24hAnkEd',
+	access_token_secret: 'vmLg9rmJ1lNDNZcBtC3X9u1PIoNlIw5BQaUKvrYdFoajM'
+});
+
+http.createServer(function(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+	client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
+		console.log(tweets);
+	});
+
+}).listen(port);
